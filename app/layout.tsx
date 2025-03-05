f6893df766e5b229bee368bd0b743d8ca2d5ad8e
ismontana <ismaotsss@gmail.com>
2025-03-04 18:30:38 -0600

    12 Create reusable modal window

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const content = (
+      <p>Hola lay</p>
+    )
+  
   return (
     <html lang="en">
       <body className={`${geistSans.variable} antialiased`}>
