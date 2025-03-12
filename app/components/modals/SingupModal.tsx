e5c2e11aa71a13d65722f25d14e3ef6e8d27c5a1
ismontana <ismaotsss@gmail.com>
2025-03-11 21:28:59 -0600

    17 Sign up

--- app/components/modals/SingupModal.tsx
+++ app/components/modals/SingupModal.tsx
@@ -3,24 +3,67 @@
 import Modal from "./Modal";
 import CustomButton from "../forms/CustomButton";
 import useSignupModal from "@/app/hooks/useSignupModal";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
 
 const SignupModal = () => {
-    const SignupModal = useSignupModal();
+    const router = useRouter()
+    const SignupModal = useSignupModal()
+    const [email, setEmail] = useState('')
+    const [password1, setPassword1] = useState('')
+    const [password2, setPassword2] = useState('')
+    const [errors, setErrors] = useState<string[]>([])
+
+    // Submit functions
+    const submitSignup = async () =>{
+        const formData = {
+            email: email,
+            password1: password1,
+            password2: password2
+        }
+
+        const response = await apiService.post('/api/auth/register/', JSON.stringify(formData)) 
+
+        if (response.access) {
+            handleLogin(response.pk, response.access, response.refresh)
+
+            SignupModal.close()
+
+            router.push('/')
+        } else {
+            const tmpErrors: string[] = Object.values(response).map((error: any) => {
+                return error;
+            })
+        }
+    }
 
     const content = (
         <>
-            <form className="space-y-4">
-                <input type="email" placeholder="Type your email" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
-                <input type="pasword" placeholder="Type your password" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
-                <input type="pasword" placeholder="Repeat your password" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
+            <form 
+            action={submitSignup}
+            className="space-y-4"
+            >
+                <input onChange={(e) => setEmail(e.target.value)} inputMode="email" type="email" placeholder="Type your email" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
+                <input onChange={(e) => setPassword1(e.target.value)} type="password" placeholder="Type your password" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
+                <input onChange={(e) => setPassword2(e.target.value)} type="password" placeholder="Repeat your password" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
 
-                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                    The error message
-                </div>
+                {errors.map((error, index) => {
+                    return (
+                        <div 
+                        key={`error_${index}`}
+                        className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
 
-                <CustomButton
+                <CustomButton 
                     label="Submit"
-                    onClick={() => { console.log('test'); }} className={""}
+                    className={""}
+                    onClick={submitSignup}
                 />
             </form>
         </>
---
+++ app/lib/actions.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { cookies } from 'next/headers';
+
+export async function handleLogin(userId: string, accessToken: string, refreshToken: string){
+    (await cookies()).set('session_userId', userId, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/'
+    })
+
+    ;(await cookies()).set('session_accessToken', accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60,
+        path: '/'
+    })
+
+    ;(await cookies()).set('session_refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/'
+    })
+}
