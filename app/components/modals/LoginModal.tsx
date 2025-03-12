38ee2ec3db76dcb8412e3e09f279bdffbd0cd4ff
ismontana <ismaotsss@gmail.com>
2025-03-11 21:40:58 -0600

    18 Log in

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -3,23 +3,60 @@
 import useLoginModal from "@/app/hooks/useLoginModal";
 import Modal from "./Modal";
 import CustomButton from "../forms/CustomButton";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
 
 const LoginModal = () => {
-    const loginModal = useLoginModal();
+    const router = useRouter()
+    const loginModal = useLoginModal()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState<string[]>([])
+
+    const submitLogin = async () => {
+        const formData = {
+            email: email,
+            password: password
+        }
+
+        const response = await apiService.post('/api/auth/login/', JSON.stringify(formData))
+
+        if (response.access) {
+            handleLogin(response.pk, response.access, response.refresh)
+
+            loginModal.close()
+
+            router.push('/')
+        } else {
+            setErrors(response.non_field_errors)
+        }
+    }
 
     const content = (
         <>
-            <form className="space-y-4">
-                <input type="email" placeholder="Type your email" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
-                <input type="pasword" placeholder="Type your password" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
+            <form
+                action={submitLogin}
+                className="space-y-4"
+            >
+                <input onChange={(e) => setEmail(e.target.value)} inputMode="email" type="email" placeholder="Type your email" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
+                <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Type your password" className="w-full h-[54px] border border-gray-300 px-4 rounded-xl" />
 
-                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                    The error message
-                </div>
+                {errors.map((error, index) => {
+                    return (
+                        <div
+                            key={`error_${index}`}
+                            className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
 
                 <CustomButton
                     label="Submit"
-                    onClick={() => { console.log('test'); }} className={""}
+                    onClick={submitLogin} className={""}
                 />
             </form>
         </>
