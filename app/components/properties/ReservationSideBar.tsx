0fa62b82e7dd089734a5b911b4469a4c00c3aa7f
ismontana <ismaotsss@gmail.com>
2025-03-12 21:10:56 -0600

    21 Detail page for properties

--- app/components/properties/ReservationSideBar.tsx
+++ app/components/properties/ReservationSideBar.tsx
@@ -1,7 +1,18 @@
-const ReservationSideBar = () => {
+export type Property = {
+    id: string
+    price_per_night: number
+}
+
+interface ReservationSideBarProps {
+    property: Property
+}
+
+const ReservationSideBar: React.FC<ReservationSideBarProps> = ({
+    property
+}) => {
     return (
         <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
-            <h2 className="mb-5 text-2xl">$200 per night</h2>
+            <h2 className="mb-5 text-2xl">${property.price_per_night} per night</h2>
             <div className="mb-6 p-3 border border-gray-400 rounded-xl">
                 <label className="mb-2 block font-bold text-xs">Guests</label>
                 <select className="w-full ml-1 text-xm">
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,7 +1,10 @@
 import ReservationSideBar from "@/app/components/properties/ReservationSideBar";
+import apiService from "@/app/services/apiService";
 import Image from "next/image";
 
-const PropertyDetailPage = () => {
+const PropertyDetailPage = async ({params}: { params: {id: string}}) => {
+    const property = await apiService.get(`/api/properties/${params.id}`)
+
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
             <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
@@ -15,36 +18,39 @@ const PropertyDetailPage = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
                 <div className="py-6 pr-6 col-span-3">
-                    <h1 className="mb-4 text-4xl">Property Name</h1>
+                    <h1 className="mb-4 text-4xl">{property.title}</h1>
 
 
                     <span className="mb-6 block text-lg text-gray-600">
-                        4 guests - 2 bedrooms - 1 bathroom
+                    {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathrooms
                     </span>
 
                     <hr />
 
                     <div className="py-6 flex items-center space-x-4">
+                    {property.landlord.avatar_url && (
                         <Image
-                            src='/profile_pic_1.jpg'
+                            src={property.landlord.avatar_url}
                             width={50}
                             height={50}
                             className="rounded-full"
                             alt="The user name"
                         />
-                        <p><strong>Ismontana</strong> is your host</p>
+                    ) || (
+                        <div className="w-12 h-12 rounded-full bg-gray-300"></div>
+                    )}
+                        <p><strong>{property.landlord.name}</strong> is your host</p>
                     </div>
                     <hr />
 
                     <p className="mt-6 text-lg">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Ea unde vel voluptas debitis dolores ad enim adipisci laudantium, 
-                        provident quam dolorem quis maxime saepe! Molestias dicta doloribus 
-                        praesentium dignissimos beatae.
+                    {property.description}
                     </p>
                 </div>
 
-                <ReservationSideBar />
+                <ReservationSideBar 
+                property={property}
+                />
             </div>
 
         </main>
