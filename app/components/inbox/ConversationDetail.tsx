139fe9e32dc08cbb7e8d8f5bf89d6b3430562dbe
ismontana <ismaotsss@gmail.com>
2025-03-14 14:52:43 -0600

    29 Connection is possible

--- app/components/inbox/ConversationDetail.tsx
+++ app/components/inbox/ConversationDetail.tsx
@@ -1,7 +1,33 @@
 'use client';
+import { ConversationType } from "@/app/inbox/page";
 import CustomButton from "../forms/CustomButton";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import { useEffect } from "react";
 
-const ConversationDetail = () => {
+interface ConversationDetailProps {
+    token: string
+    userId: string
+    conversation: ConversationType
+}
+
+const ConversationDetail: React.FC<ConversationDetailProps> = ({
+    token,
+    userId,
+    conversation
+}) => {
+    const myUser = conversation.users?.find((user) => user.id == userId)
+    const otherUser = conversation.users?.find((user) => user.id != userId)
+
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(`ws://127.0.0.1:8000/ws/${conversation.id}/?token=${token}`, {
+            share: false,
+            shouldReconnect: () => true,
+        },
+    )
+
+    useEffect(() => {
+        console.log('Conexión cambiada', readyState)
+    }, [readyState])
+
     return (
         <>
             <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
@@ -19,15 +45,15 @@ const ConversationDetail = () => {
                 </div>
             </div>
             <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
-                <input 
-                type="text" 
-                placeholder="Text your message..."
-                className="w-full p-2 bg-gray-200 rounded-xl"
+                <input
+                    type="text"
+                    placeholder="Text your message..."
+                    className="w-full p-2 bg-gray-200 rounded-xl"
                 />
-                <CustomButton 
-                label = 'Send'
-                onClick={() => console.log('Send message')}
-                className="w-[100px]"
+                <CustomButton
+                    label='Send'
+                    onClick={() => console.log('Send message')}
+                    className="w-[100px]"
                 />
             </div>
         </>
--- app/myfavorites/page.tsx
+++ app/myfavorites/page.tsx
@@ -5,7 +5,7 @@ import { getUserId } from "../lib/actions"
 const MyFavoritePage = async () => {
     const userId = await getUserId()
 
-    if (!userId){
+    if (!userId){ 
         return(
             <main className="max-w-[1500px] mx-auto px-6 py-12">
                 <p>You need to be authenticated...</p>
